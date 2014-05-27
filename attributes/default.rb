ee289b064d3590885dd21a59d3d61fa3a20f1277
Matt Mencel <mr-mencel@wiu.edu>
2014-05-27 12:35:35 -0500

    ldap attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,9 @@ default[:ejabberd][:domain] = 'ejabberd.example.com'
 default[:ejabberd][:prefix] = '/opt/ejabberd'
 
 default[:ejabberd][:jabber_domain] = node[:fqdn]
+
+default[:ldap][:base] = 'dc=example,dc=com'
+default[:ldap][:filter] = '(objectClass=shadowAccount)'
+default[:ldap][:port] = '389'
+default[:ldap][:servers] = 'ldap.example.com'
+default[:ldap][:uids] = '{"mail", "%u@mail.example.org"}'
